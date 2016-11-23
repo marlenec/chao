5472412742d5dcb8b45fb4923663e184ca326ec4
marlenec <marlene.chiarello@umontpellier.fr>
2016-11-23 09:02:22 +0100

    Add files via upload

--- chao_alpha_beta_v2.0.r
+++ chao_alpha_beta_v2.0.r
@@ -2,7 +2,8 @@
 # FUNCTION to compute Chao's alpha and beta diversity, expressed as Hill Numbers,
 # for a set of communities
 #
-# This function requires the installation of packages entropart (REF PACKAGE  + papier MARCON)
+# This function requires the installation of packages entropart (Marcon & Hérault, 2015 ; Marcon et al.,
+# PloS One, 2014)
 #
 # Arguments :
 # - matrix : a matrix with raw or relative abundances of species (columns) in
@@ -14,13 +15,8 @@
 #
 # - q : value(s) for the q parameter, among 0,1,2. Default is q=c(0,1,2)
 #       See Chao et al. 2014 for explanations about the q parameter.
+#
 # - run_example : if TRUE, a simple example provided at the end of this script is runned.
-#
-#
-# NB : The conversion of a tree from the phylo format into the phylog format is easy :
-# write.tree(tree, file="tree.txt") # 1°/ you need to make a .txt file in a newick format
-# tree<-paste(readLines('tree.txt')) # 2°/ you need to read it as a character string
-# tree_phylog<-newick2phylog(tree) # 3°/ newick2phylog{ade4} converts the character string into a phylog object
 #
 #
 # Details :
@@ -29,8 +25,8 @@
 # - If tree_phylog is provided, the phylogenetic diversity is calculated.
 # - Chao's phylogenetic diversity can be calculated on non-ultrametric trees, but the meaning is not clear.
 #   If a non-ultrametric is provided, phylogenetic diversity will be calculated an returned with a warning.
-# - If raw abundances are provided, unequal number of indivdi is allowed. In this case, the total abundance
-#   in each community will be taken into account when calculating pairwise beta diversity.
+# - If raw abundances are provided, unequal numbers of individuals between communities are allowed. In this case,
+#   the total abundance in each community will be taken into account when calculating pairwise beta diversity.
 #
 #
 # OUTPUTS : a list with for each biodiversity facet (i.e. taxo and phylo):
